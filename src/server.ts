7a0ebf61e588c30eaeb98da99c2c14b2791b1bb5
Mohammad Saiful Islam <saifmdislam231@gmail.com>
2024-03-19 11:50:48 +0600

    server project initilaize with express typescript and prisma

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from "express";
+const app = express();
+const port = 3333;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(port, () => {
+  console.log(`HealthCare server is listening on port ${port}`);
+});
