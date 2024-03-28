658dbfdae7d495ca391bd457e613618684cb1f24
Mohammad Saiful Islam <saifmdislam231@gmail.com>
2024-03-28 09:43:57 +0600

    optimize search filtering functionality added

--- src/app/modules/admin/admin.service.ts
+++ src/app/modules/admin/admin.service.ts
@@ -1,16 +1,27 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 const getAllAdminFromDb = async (params: any) => {
-  console.log(params);
+  let andConditions: Prisma.AdminWhereInput[] = [];
+
+  const adminSearchableFields = ["name", "email"];
+
+  if (params.searchTerm) {
+    andConditions.push({
+      OR: adminSearchableFields.map((field) => ({
+        [field]: {
+          contains: params.searchTerm,
+          mode: "insensitive",
+        },
+      })),
+    });
+  }
+
+  const whereConditions: Prisma.AdminWhereInput = { AND: andConditions };
+
   const result = await prisma.admin.findMany({
-    where: {
-      name: {
-        contains: params.searchTerm,
-        mode: "insensitive",
-      },
-    },
+    where: whereConditions,
   });
   return result;
 };
