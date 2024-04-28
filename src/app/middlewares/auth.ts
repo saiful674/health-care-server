f1c371f1ba0808206cc1ba723baf6705f411c8da
Mohammad Saiful Islam <saifmdislam231@gmail.com>
2024-04-28 12:44:48 +0600

    auth middleware and AppError class added

---
+++ src/app/middlewares/auth.ts
@@ -0,0 +1,40 @@
+import { UserStatus } from "@prisma/client";
+import { NextFunction, Request, Response } from "express";
+import httpStatus from "http-status";
+import { JwtPayload, Secret } from "jsonwebtoken";
+import config from "../../config";
+import prisma from "../../utils/prisma";
+import AppError from "../errors/AppError";
+import { jwtHalper } from "../halpers/jwtHalper";
+
+const auth = (...roles: string[]) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    const token = req.headers.authorization;
+    try {
+      if (!token) {
+        throw new AppError(httpStatus.UNAUTHORIZED, "You are not authorized!");
+      }
+      const decodedUser = jwtHalper.varifyToken(
+        token,
+        config.jwt.jwt_secret as Secret
+      ) as JwtPayload;
+
+      if (roles.length && !roles.includes(decodedUser.role)) {
+        throw new AppError(httpStatus.FORBIDDEN, "Access Forbidden!");
+      }
+
+      const isUserExist = await prisma.user.findUniqueOrThrow({
+        where: {
+          email: decodedUser.email,
+          status: UserStatus.active,
+        },
+      });
+
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
+};
+
+export default auth;
--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -1,8 +1,14 @@
+import { UserRole } from "@prisma/client";
 import express from "express";
+import auth from "../../middlewares/auth";
 import { userControllers } from "./user.controller";
 
 const router = express.Router();
 
-router.post("/", userControllers.createAdmin);
+router.post(
+  "/",
+  auth(UserRole.admin, UserRole.superAdmin),
+  userControllers.createAdmin
+);
 
 export const userRouter = router;
